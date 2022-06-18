76b16894dc1efa10dd03bb91a8afeb0f86569d7a
Snow <kibbewater@outlook.com>
2022-06-18 21:44:53 +0000

    Begun work on Ticket API for DB

---
+++ api/tickets.js
@@ -0,0 +1,74 @@
+const { Guild, TextChannel, GuildMember, Channel } = require('discord.js');
+const { ObjectId } = require('mongodb');
+
+const mongo = require('../private/db').getInstance();
+
+/**
+ * Create a ticket
+ * @param {Guild} guild
+ * @param {TextChannel} channel
+ * @param {GuildMember} author
+ * @param {GuildMember} claimant
+ * @returns {Object}
+ */
+function _createTicket(guild, channel, author, claimant) {
+	const tickets = mongo.db('ticketeer').collection('tickets');
+
+	const localId = _getLocalId(guild);
+
+	return tickets.insertOne({
+		localId,
+		guildId: guild.id,
+		channelId: channel.id,
+		authorId: author.id,
+		claimId: claimant.id,
+	});
+}
+
+/**
+ * Get the tickets for a guild
+ * @param {Guild} guild
+ * @returns {Object}
+ */
+function _getTickets(guild) {
+	const tickets = mongo.db('ticketeer').collection('tickets');
+
+	// get the latest ticket with the guildId of the guild
+	return tickets.find({ guildId: guild.id }).toArray();
+}
+
+/**
+ * Close the ticket with the given id
+ * @param {ObjectId} id
+ */
+function _closeTicket(id) {
+	const tickets = mongo.db('ticketeer').collection('tickets');
+
+	tickets.updateOne({ _id: id }, { $set: { closedAt: new Date() } });
+}
+
+/**
+ * Close the ticket with the given channelId
+ * @param {Channel} channel
+ */
+function _closeTicketChannel(channel) {
+	const tickets = mongo.db('ticketeer').collection('tickets');
+
+	tickets.updateOne({ channelId: channel.id }, { $set: { closedAt: new Date() } });
+}
+
+/**
+ * Get the guild's next localId
+ * @param {Guild} guild
+ * @returns {Number}
+ */
+function _getLocalId(guild) {
+	return mongo.db('ticketeer').collection('tickets').countDocuments({ guildId: guild.id }) + 1;
+}
+
+module.exports = {
+	createTicket: _createTicket,
+	getTickets: _getTickets,
+	closeTicket: _closeTicket,
+	closeTicketChannel: _closeTicketChannel,
+};
